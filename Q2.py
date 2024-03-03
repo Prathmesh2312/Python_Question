6b52aea702d3c4e17b4cd9735a712c40ce17be5c
Prathmesh2312 <praut2312@gmail.com>
2024-03-03 20:12:41 +0530

    Area of Rectangle

---
+++ Q2.py
@@ -0,0 +1,7 @@
+#Create a program that calculates the area of a rectangle using length and width stored in variables.
+
+Length = 5
+Width = 12
+print("Length is:",Length,"\nWidth is:",Width)
+Area = Length * Width
+print("Area of Rectangle is:",Area)
