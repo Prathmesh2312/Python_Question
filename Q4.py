7b636314c3ef63ad42d9acbc69ea87eb6cc41132
Prathmesh2312 <praut2312@gmail.com>
2024-03-04 20:49:42 +0530

    converts temperature from Celsius to Fahrenheit

---
+++ Q4.py
@@ -0,0 +1,5 @@
+#Develop a program that converts temperature from Celsius to Fahrenheit using variables.
+temp = float(input("Enter Temperature in Celsius:"))
+print("Temperature is :",temp,"Celsius")
+fahr = (temp * 9/5) + 32
+print("Temperature is :",fahr,"Fahrenheit")
