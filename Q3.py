769b9b6c63f2dc9917845da162d34c48e01ad8ee
Prathmesh2312 <praut2312@gmail.com>
2024-03-04 20:48:49 +0530

    Average of three numbers

---
+++ Q3.py
@@ -0,0 +1,12 @@
+#Write a Python program to calculate the average of three numbers and store the result in a variable.
+a = 10
+b = 20 
+c = 25
+average = a+b+c/3
+print("Average of three variables:",average)
+#OR
+p = int(input("Enter first Number:"))
+q = int(input("Enter second Number:"))
+r = int(input("Enter third Number:"))
+Averave = p+q+r/3
+print("Average is:",Averave)
