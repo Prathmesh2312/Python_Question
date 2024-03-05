2a2be10819e6677a2c53a4790cfd07123b522b6e
Prathmesh2312 <praut2312@gmail.com>
2024-03-05 23:13:45 +0530

    sum, product, and quotient using variables

---
+++ Q5.py
@@ -0,0 +1,10 @@
+# Create a Python program that takes two integers as input and prints their sum, product, and quotient using variables.
+a = int(input("Enter first no:"))
+b = int(input("Enter Second no:"))
+sum = a + b
+product = a * b
+quotient = a / b 
+print("Sum of two no's:",sum)
+print("Product of two no's:",product)
+print("Quotient of two no's:",quotient)
+
