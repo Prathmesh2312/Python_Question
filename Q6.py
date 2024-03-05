e4cf86998b25d510bfddccb796949ea3365eb78d
Prathmesh2312 <praut2312@gmail.com>
2024-03-05 23:14:39 +0530

    program to find the maximum and minimum elements in a list

---
+++ Q6.py
@@ -0,0 +1,7 @@
+#Write a Python program to find the maximum and minimum elements in a list.
+list = [1,8,2,6,3,4,7,9,4,2,63]
+max = max(list)
+min = min(list)
+print(list)
+print("Maximum number from the list is:",max)
+print("Minimum number from the list is:",min)
