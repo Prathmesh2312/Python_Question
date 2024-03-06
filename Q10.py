e9345f36ee8d617164307564a7560432d7aa2bbe
Prathmesh2312 <praut2312@gmail.com>
2024-03-06 20:40:03 +0530

    capitalizes the first letter of each word

---
+++ Q10.py
@@ -0,0 +1,5 @@
+#Develop a program that takes a sentence as input and capitalizes the first letter of each word.
+import string
+string1 = input("Enter a sentence:")
+capital = string.capwords(string1)
+print(capital)
