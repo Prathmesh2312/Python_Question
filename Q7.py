70b3f4aa0e9128e1afc68a6137acda0ad48e8003
Prathmesh2312 <praut2312@gmail.com>
2024-03-06 12:21:31 +0530

    occurrences of a substring in a given string

---
+++ Q7.py
@@ -0,0 +1,6 @@
+#Write a Python function to count the number of occurrences of a substring in a given string.
+string = "Hi, My name is Prathmesh Raut. Currently I am learning Python Programming from basics."
+substring_count = string.count("name")
+print(string)
+print("Substring Count:",substring_count)
+
