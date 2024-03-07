97665ea479b614b430d9d53453f1ba16cffd0d5e
Prathmesh2312 <praut2312@gmail.com>
2024-03-07 22:30:44 +0530

    check if a given number is even or odd using boolean expressions

---
+++ Q12.py
@@ -0,0 +1,6 @@
+#Write a Python program to check if a given number is even or odd using boolean expressions.
+number = int(input("Enter a Number:"))
+if (number % 2)==0 :
+    print(number,"is Even")
+else:
+    print(number,"is Odd")
