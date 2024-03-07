d1e057f1df3b185ebf00b7d9d7c67c228b2d67b3
Prathmesh2312 <praut2312@gmail.com>
2024-03-07 22:31:56 +0530

    checks if a given list is empty or not

---
+++ Q14.py
@@ -0,0 +1,6 @@
+#Develop a program that checks if a given list is empty or not.
+list = [1,2,3,4,5,6,4,5]
+if(len(list) == 0):
+    print("Given List is Empty")
+else:
+    print("Given List is Not Empty")
