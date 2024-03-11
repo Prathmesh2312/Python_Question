689ae92f080514fd55c900928e54122f522ec6e0
Prathmesh2312 <praut2312@gmail.com>
2024-03-11 20:27:15 +0530

    Area of circle

---
+++ Q16.py
@@ -0,0 +1,4 @@
+#Write a Python program to calculate the area of a circle using the radius provided by the user.
+radius = float(input("Enter Radius of Circle:"))
+area_of_circle = 2 * 3.14 * radius
+print("Area of Circle is:",area_of_circle,"squ")
